49d7725a9e610e46378b670292b6e7bb15763096
bodzia <bpaczkowska@hotmail.com>
2018-10-01 12:01:12 +0200

    my first commit

---
+++ 04-operatory.py
@@ -0,0 +1,9 @@
+a = 10
+b = 4
+
+print("{} + {} = {}".format(a, b, a + b))
+print("{} - {} = {}".format(a, b, a - b))
+print("{} * {} = {}".format(a, b, a * b))
+print("{} / {} = {}".format(a, b, a / b))
+print("{} // {} = {}".format(a, b, a // b))
+print("{} % {} = {}".format(a, b, a % b))
